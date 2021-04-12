59388d74b28c2439064ede96d7813ebd7b58e418
shaw-555 <shaw18664590570@163.com>
2021-04-12 21:23:49 +0800

    ff

--- 20smallProjects/src/components/projectOne/projectOne.vue
+++ 20smallProjects/src/components/projectOne/projectOne.vue
@@ -15,12 +15,12 @@
       </div>
       <div class="form-control">
         <label for="password">密码</label>
-        <input type="password" id="password" placeholder="请输入密码">
+        <input type="password" id="password" placeholder="请输入密码" @blur="checkLength()">
         <small>错误提示</small>
       </div>
       <div class="form-control">
         <label for="password2">确认密码</label>
-        <input type="password" id="password2" placeholder="请输入用户名">
+        <input type="password" id="password2" placeholder="请输入用户名" @blur="matchPassWord()">
         <small>错误提示</small>
       </div>
       <button>提交</button>
@@ -44,6 +44,10 @@ export default {
     checkName () {
       if ((document.getElementById('username').value).trim() === '') {
         alert('此项为必填')
+        let nodeList = document.querySelectorAll('.form-control')
+        nodeList.forEach((i) => {
+          document.querySelector('.form-control').classList.add('error')
+        })
       }
     },
     checkEmail () {
@@ -53,8 +57,27 @@ export default {
       } else {
         if (!re.test((document.getElementById('email').value).trim())) {
           alert('邮箱格式错误')
+          let nodeList = document.querySelectorAll('.form-control')
+          nodeList.forEach((i) => {
+            document.querySelector('.form-control').classList.add('error')
+          })
         }
       }
+    },
+    checkLength () {
+      const min = 10
+      const max = 20
+      const value = document.getElementById('password').value
+      if (value.trim().length < min || value.trim().length > max) {
+        alert(`密码应当大于${min}个字符且小于${max}个字符`)
+      } else {
+        console.log('密码合法')
+      }
+    },
+    matchPassWord () {
+      const value1 = document.getElementById('password').value
+      const value2 = document.getElementById('password2').value
+      value1 === value2 ? console.log('两次密码一样') : alert('两次密码不相同11111111111')
     }
   }
 }
